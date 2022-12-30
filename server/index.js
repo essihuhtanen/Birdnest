47f8dc99f8c59a7a84cb99cdd7c91ed16b742df3
Essi <essi.huhtanen@tuni.fi>
2022-12-30 13:08:47 +0200

    Add cors to server and allow all origins

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 3001
+
+app.use(
+  cors({
+    origin: '*'
+  })
+)
 
 app.get('/drones', (req, res) => {
   fetch('http://assignments.reaktor.com/birdnest/drones')
