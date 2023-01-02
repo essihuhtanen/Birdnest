5c0d50262939c3915f01e9cabbe77cbbd5c59486
Essi <essi.huhtanen@tuni.fi>
2023-01-02 15:29:37 +0200

    Move pilot-related functions from infoStore to pilotParser

--- client/src/utils/pilotParser.ts
+++ client/src/utils/pilotParser.ts
@@ -1,6 +1,12 @@
 import { Drone, Pilot } from '../types'
+import { fetcher } from './fetcher'
 
-export const pilotParser = (data: string, drone: Drone) => {
+const fetchPilot = async (drone: Drone) => {
+  const data = await fetcher({ path: 'pilots', id: drone.serialNumber })
+  return parsePilot(data, drone)
+}
+
+export const parsePilot = (data: string, drone: Drone) => {
   const jsonData = JSON.parse(data)
 
   const pilot: Pilot = {
@@ -16,4 +22,16 @@ export const pilotParser = (data: string, drone: Drone) => {
   Object.values(pilot).forEach((value) => value === undefined && (correctData = false))
 
   return correctData ? pilot : null
+}
+
+export const pilotParser = async (drones: Drone[]) => {
+  const pilots: Pilot[] = []
+
+  for (const drone of drones) {
+    const pilot = await fetchPilot(drone)
+    console.log('pilot fetched: ' + pilot?.email)
+    pilot !== null && pilots.push(pilot)
+  }
+
+  return pilots
 }
