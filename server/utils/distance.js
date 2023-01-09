338b6709721781a8fd4303bd9c68e07c65eddd16
Essi <essi.huhtanen@tuni.fi>
2023-01-09 17:38:33 +0200

    Move parsing XML to backend side and add new endpoint for drone history data.

--- server/utils/distance.js
+++ server/utils/distance.js
@@ -1,7 +1,7 @@
 const ORIGO_X = 250000
 const ORIGO_Y = 250000
 
-export const distanceFromNest = (x: number, y: number) => {
+export const distanceFromNest = (x, y) => {
   const xDist = x - ORIGO_X
   const yDist = y - ORIGO_Y
   return Math.sqrt(xDist * xDist + yDist * yDist) / 1000
